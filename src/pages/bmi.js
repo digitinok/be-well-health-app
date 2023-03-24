a96b107e1cf6c7e40b36b88c322322ea4f219f14
digitinok <75326757+digitinok@users.noreply.github.com>
2023-03-24 00:51:21 +0000

    BMI chart

--- src/pages/bmi.js
+++ src/pages/bmi.js
@@ -1,9 +1,13 @@
+import DynamicPlot from "../components/DynamicPlot"
 
 export default function bmi() {
+
+  
     return (
       <div>
         <h1>BMI Calculator</h1>
         <p>Hi.</p>
+        <DynamicPlot />
       </div>
     )
   }
