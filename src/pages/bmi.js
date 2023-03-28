4b1466360377262777f2c84591966049958f507b
Mukabya <jjingog@aol.com>
2023-03-28 20:54:03 +0100

    calculation function for bmi

--- src/pages/bmi.js
+++ src/pages/bmi.js
@@ -1,14 +1,30 @@
+let calcBMI =(Weight, height) => {
+  // prevent submitting event. prevent Default{}
+    if (weight===0 || height===0) {
+      alert ("please enter a valid weight and height")
+      
+    }
+  else{
+    return (weight/(height/100)**2).toFixed(2)
+    
+  }
+}
 
 export default function bmi() {
-    return (
-        <div>
-          <h1>BMI Calculator</h1>
-          <p>Height (in cm)</p>
-          <input type="text" />
-          <p>Weight (in Kg)</p>
-          <input type="text"/>
-          
+  return (
+    <div>
+      <h1>BMI Calculator</h1>
+      <p>Height (in cm)</p>
+      <input type="text" />
+      <p>Weight (in Kg)</p>
+      <input type="text" />
+      
+      
+      
 
-        </div>
-    );
-  }
+
+
+
+    </div>
+  );
+}
